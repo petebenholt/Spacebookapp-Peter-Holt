2685be676b4230cd3afce1283aa03dda2f3c6f95
petebenholt <peteholt1999@gmail.com>
2022-02-17 22:41:15 +0000

    login screen added

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Button, ScrollView, TextInput, StyleSheet, Text} from 'react-native';
+//import {  ScrollView, TextInput } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class LoginScreen extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    login = async () => {
+
+        //Validation here...
+
+        return fetch("http://localhost:3333/api/1.0.0/login", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 400){
+                throw 'Invalid email or password';
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .then(async (responseJson) => {
+                console.log(responseJson);
+                await AsyncStorage.setItem('@session_token', responseJson.token);
+                this.props.navigation.navigate("Home");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    render(){
+        return (
+            <View style= {styles.container}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
+                  <View style= {styles.box}>
+                    <Text style= {styles.text}>Spacebook</Text>
+                    <TextInput
+                        placeholder="Enter your email..."
+                        placeholderTextColor= 'white'
+                        onChangeText={(email) => this.setState({email})}
+                        value={this.state.email}
+                        style={{padding:5, borderWidth:1, margin:5}}
+                        borderColor= "black"
+                    />
+                    <TextInput
+                        placeholder="Enter your password..."
+                        placeholderTextColor= 'white'
+                        onChangeText={(password) => this.setState({password})}
+                        value={this.state.password}
+                        secureTextEntry
+                        borderColor= "black"
+                        style={{padding:5, borderWidth:1, margin:5}}
+                    />
+                    <Button
+                        title="Login"
+                        color="rgb(32,32,32)"
+                        onPress={() => this.login()}
+                    />
+                    <Button
+                        title="Don't have an account?"
+                        color="darkred"
+                        onPress={() => this.props.navigation.navigate("Signup")}
+                    />
+                  </View>
+                </ScrollView>
+              </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: 'rgb(32,32,32)',
   },
-});
+  text: {
+    color: 'white',
+    //fontWeight: 'bold',
+    marginLeft: 138,
+  },
+  box: {
+    backgroundColor: 'rgb(64,64,64)',
+    padding: 20,
+
+  }
+})
+
+
+export default LoginScreen;
